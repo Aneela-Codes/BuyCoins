bed224912251fe1689b44b6eb03dc89b19e36a8f
Aneela-Codes <63118445+Aneela-Codes@users.noreply.github.com>
2020-11-14 18:08:52 +0500

    adding

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const openMenu=()=>{
+    let navbar =document.getElementById('navbar2');
+    navbar.style.display='block'
+}
+const closeMenu=()=>{
+    let navbar =document.getElementById('navbar2');
+    navbar.style.display='none'
+}
